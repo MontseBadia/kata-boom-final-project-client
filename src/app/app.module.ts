35cbe8fd64e717bf314c16b0cc7181eb12f4abed
Montse Badia <mbadia_11@hotmail.com>
2018-08-01 21:55:09 +0200

    implemented signup form, validated the form, made post to backend from auth service and changed general css and layout

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '../../node_modules/@angular/common/http';
+
+// ----- COMPONENTS ------
+import { AppComponent } from './app.component';
+
+// ----- PAGES ------
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
+
+// ----- SERVICES ------
+import { AuthService } from './services/auth.service';
+
+// ----- ROUTES ------
+const routes: Routes = [
+  { path: 'signup', component: SignupPageComponent },
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginPageComponent,
+    SignupPageComponent,
+    ProfilePageComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule
+  ],
+  providers: [
+    AuthService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- src/app/pages/signup-page/signup-page.component.ts
+++ src/app/pages/signup-page/signup-page.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '../../../../node_modules/@angular/router';
+
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-signup-page',
   templateUrl: './signup-page.component.html',
   styleUrls: ['./signup-page.component.css']
 })
+
 export class SignupPageComponent implements OnInit {
 
-  constructor() { }
+  feedbackEnabled = false;
+  error = null;
+  processing = false;
+  username: string;
+  password: string;
+
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
 
+  submitForm(form) {
+    this.error = '';
+    this.feedbackEnabled = true;
+    if (form.valid) {
+      this.processing = true;
+      this.authService.signup(this.username, this.password)
+        .then((result) => {
+          // ... handle result, reset form, etc...
+          this.router.navigate(['/profile']);
+        })
+        .catch((err) => {
+          this.error = err.error;
+          this.processing = false;
+          this.feedbackEnabled = false;
+        });
+    }
+  }
+
 }
+
+
+
