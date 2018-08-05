d72744fe3ec8c0c1d9dae43bf19e20cba75e12ac
Montse Badia <mbadia_11@hotmail.com>
2018-08-05 15:11:25 +0200

    created list component to list all katasof a user in profile page

---
+++ src/app/components/list/list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+
+export class ListComponent implements OnInit {
+
+  @Input() katas: any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
