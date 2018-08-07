5e57aaa18dd8d2e1c7e3c5fd0a845a4cffbbe827
Montse Badia <mbadia_11@hotmail.com>
2018-08-07 16:37:12 +0200

    refactored code in kata component page ts

--- src/app/pages/kata-page/kata-page.component.ts
+++ src/app/pages/kata-page/kata-page.component.ts
@@ -14,7 +14,6 @@ import { UserService } from '../../services/user.service';
   templateUrl: './kata-page.component.html',
   styleUrls: ['./kata-page.component.css']
 })
-
 export class KataPageComponent implements OnInit {
 
   // DON'T FORGET TO DECLARE ARRAY AS [ ] --> MEMORY CRASHES!!
@@ -25,7 +24,6 @@ export class KataPageComponent implements OnInit {
   alreadySubmitted = false;
   feedbackEnabled = false;
   error = null;
-  randomKataId: any;
   passedTests: boolean;
   emptyEditor: boolean;
   randomKataResults: [any];
@@ -40,36 +38,79 @@ export class KataPageComponent implements OnInit {
   constructor(private route: ActivatedRoute, private kataService: KataService, private userService: UserService, private router: Router) { }
 
   ngOnInit() {
-    this.route.params
-      .subscribe((kataName) => {
-        if (kataName.name === 'no-more-katas') { // --- IF USER HAS DONE ALL AVAILABLE KATAS
-          this.noKatas = 'Sorry! We run out of katas for today';
-        } else {
-          this.kataService.getOne(kataName)
-            .then((kata) => {
-              this.randomKata = kata;
-              this.randomKata.functionName = kata.functionName;
-              this.randomKata.name = this.randomKata.name.replace(/-/g, ' '); // --- REPLACE DASHES OF FUNCTION NAME
-              this.randomKata.parameters = kata.parameters;
-              this.text = `function ${this.randomKata.functionName} (${this.randomKata.parameters}) {
+    this.route.params.subscribe((kataName) => {
+      if (kataName.name === 'no-more-katas') { // --- IF USER HAS DONE ALL AVAILABLE KATAS
+        this.noKatas = 'Sorry! We run out of katas for today';
+        return;
+      }
+      this.loadKata(kataName);
+    });
+  }
+
+  private loadKata(kataName) {
+    this.kataService.getOne(kataName)
+      .then((kata) => {
+        this.randomKata = kata;
+        this.randomKata.functionName = kata.functionName;
+        this.randomKata.name = this.randomKata.name.replace(/-/g, ' '); // --- REPLACE DASHES OF FUNCTION NAME
+        this.randomKata.parameters = kata.parameters;
+        this.text = `function ${this.randomKata.functionName} (${this.randomKata.parameters}) {
 
 }`;
-              if (this.randomKata.tests[0].params.length === 1) {
-                this.oneParameter = true;
-              } else {
-                this.oneParameter = false;
-              }
-              if (typeof (this.randomKata.tests[0].result) === 'string') {
-                for (let x = 0; x < this.randomKata.tests.length; x++) {
-                  this.randomKata.tests[x].result = '"' + this.randomKata.tests[x].result + '"';
-                }
-              }
-            })
-            .catch((err) => {
-              this.router.navigate(['/**']); // Is this the correct way?
-            });
+        if (this.randomKata.tests[0].params.length === 1) {
+          this.oneParameter = true;
+        } else {
+          this.oneParameter = false;
+        }
+        if (typeof (this.randomKata.tests[0].result) === 'string') {
+          for (let x = 0; x < this.randomKata.tests.length; x++) {
+            this.randomKata.tests[x].result = '"' + this.randomKata.tests[x].result + '"';
+          }
+        }
+      })
+      .catch((err) => {
+        this.router.navigate(['/**']); // Is this the correct way?
+      });
+  }
+
+  private save(randomKataId) {
+    this.feedbackEnabled = true;
+    this.randomKataResults = undefined;
+
+    this.kataService.checkKata(this.inputCode, randomKataId)
+      .then((data) => {
+        this.checkAndSubmit(randomKataId, data);
+      })
+      .catch((err) => {
+        this.testAndSubmit = false;
+        this.passedTests = false;
+        this.ableToSubmit = false;
+        this.error = err.error;
+        if (this.inputCode !== '') {
+          this.emptyEditor = false;
         }
       });
+  }
+
+  private checkAndSubmit(randomKataId, data) {
+    this.randomKataResults = data.evaluation;
+    this.finalStatus = data.finalStatus;
+    for (let x = 0; x < this.randomKataResults.length; x++) {
+      if (typeof (this.randomKataResults[x]) === 'string') {
+        this.randomKataResults[x] = '"' + this.randomKataResults[x] + '"';
+      }
+    }
+    if (data.isCorrect) {
+      this.passedTests = true;
+      this.ableToSubmit = true;
+      if (this.testAndSubmit === true) {
+        return this.userService.submitKata(this.inputCode, randomKataId)
+          .then(() => {
+            this.alreadySubmitted = true;
+            this.ableToSubmit = false;
+          });
+      }
+    }
   }
 
   onChange(inputCode) {
@@ -84,40 +125,7 @@ export class KataPageComponent implements OnInit {
     this.alreadySubmitted = false;
 
     if (form.valid) {
-      this.randomKataId = randomKataId; // Do I need this?
-      this.feedbackEnabled = true;
-      this.randomKataResults = undefined;
-
-      this.kataService.checkKata(this.inputCode, this.randomKataId)
-        .then((data) => {
-          this.randomKataResults = data.evaluation;
-          this.finalStatus = data.finalStatus;
-          for (let x = 0; x < this.randomKataResults.length; x++) {
-            if (typeof (this.randomKataResults[x]) === 'string') {
-              this.randomKataResults[x] = '"' + this.randomKataResults[x] + '"';
-            }
-          }
-          if (data.isCorrect) {
-            this.passedTests = true;
-            this.ableToSubmit = true;
-            if (this.testAndSubmit === true) {
-              this.userService.submitKata(this.inputCode, this.randomKataId)
-                .then(() => {
-                  this.alreadySubmitted = true;
-                  this.ableToSubmit = false;
-                });
-            }
-          }
-        })
-        .catch((err) => {
-          this.testAndSubmit = false;
-          this.passedTests = false;
-          this.ableToSubmit = false;
-          this.error = err.error;
-          if (this.inputCode !== '') {
-            this.emptyEditor = false;
-          }
-        });
+      this.save(randomKataId);
     }
   }
 }
