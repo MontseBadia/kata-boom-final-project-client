6628800e84b3537fee28f1cd095748ff59d82cb3
Montse Badia <mbadia_11@hotmail.com>
2018-08-07 10:48:53 +0200

    added option to show comments of a kata inside a kata card, and called the retrieve kata comments function from the user service

--- src/app/components/kata-card/kata-card.component.ts
+++ src/app/components/kata-card/kata-card.component.ts
@@ -15,20 +15,41 @@ export class KataCardComponent implements OnInit {
   @Input() kata: any;
   @Input() userId: any;
   @Input() myOwnKatas: any;
+  @Input() myId: any;
 
+  showAddComment = false;
   showComment = false;
+  kataUserId: any;
+  userKatas: any;
 
   constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit() {
+    if (this.userId) {
+      this.kataUserId = this.userId;
+    } else {
+      this.kataUserId = this.myId;
+    }
+
     this.kata.kata.name = this.kata.kata.name.replace(/-/g, ' '); // --- REPLACE DASHES OF FUNCTION NAME
-    console.log(this.myOwnKatas);
+
+    this.userService.retrieveKataComments(this.kataUserId, this.kata._id)
+      .then((katas) => {
+        this.userKatas = katas.katas[0];
+        if (this.userKatas.length === 0) {
+          this.userKatas = null;
+        }
+
+        console.log(this.userKatas);
+      })
+      .catch(() => {
+        this.router.navigate(['/**']);
+      });
   }
 
   handleAddComment($event) {
     this.userService.addComment($event.comment, $event.userId, $event.kataId)
-      .then((user) => {
-        console.log(user);
+      .then(() => {
       })
       .catch(() => {
         this.router.navigate(['/**']);
